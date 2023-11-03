372962d27dbafe97bc2d0ceca930fcd59a8bf2e2
Marco Vasile <127522166+marukooo96@users.noreply.github.com>
2023-11-03 22:20:09 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+// //const sqlite3 = require('sqlite3');
+// //const db = new sqlite3.Database('./turisti.db');
+
+//     // prepara query sql
+//     var stmt = db.prepare('INSERT INTO sicilia (provincia, mese, n) VALUES (?, ?, ?)');
+//     // esegui query con parametri corretti
+//     stmt.run(provincia, mese, numero, function(err) {
+//         if (err) {
+//             alert("Errore durante l'inserimento dei dati: " + err.message);
+//         } else {
+//             alert("Dati inseriti con successo");
+//         }
+//     });
+//     // chiudi dichiarazione
+//     stmt.finalize();
+
+
